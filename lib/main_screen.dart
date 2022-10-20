b9b52c9371cc88cd992f4aa8dcce083fb137a0f6
Artem Barkalov <artembark@gmail.com>
2022-10-21 02:58:05 +0300

    navigator fix, cookies fix, tab bar view, announcements

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,43 +1,64 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pita/business_page.dart';
+import 'package:pita/home_page.dart';
+import 'package:pita/school_page.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+  const MainScreen({super.key, required this.client});
+
+  final Dio client;
 
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
+class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
+  late final TabController tabController;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  @override
+  void initState() {
+    tabController = TabController(
+      length: 3,
+      initialIndex: 0,
+      vsync: this,
+    );
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Pita")),
-        body: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'School',
-            ),
+        appBar: AppBar(
+          title: const Text("Pita"),
+          bottom: TabBar(
+            controller: tabController,
+            tabs: const [
+              Tab(
+                text: 'Home',
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                text: 'Business',
+                icon: Icon(Icons.business),
+              ),
+              Tab(
+                text: 'School',
+                icon: Icon(Icons.school),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          controller: tabController,
+          children: [
+            const HomePage(),
+            const BusinessPage(),
+            SchoolPage(
+              client: widget.client,
+            )
           ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
         ));
   }
 }
